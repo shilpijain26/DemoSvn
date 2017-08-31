e733083879216bc035898236b2689066df443ab1
shilpijain26 <shilpi@quarterpie.com>
2017-08-31 15:05:38 +0530

    Testing1

--- app/src/main/java/com/dk/demosvn/MainActivity.java
+++ app/src/main/java/com/dk/demosvn/MainActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn , btn1;
-
+    Button btn , btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
